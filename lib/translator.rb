e42fed85548561bd0b312d5738a56b23169220a4
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-21 01:37:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(emoticon_files)
   emoticons = YAML.load_file('./lib/emoticons.yml')
   new_hash = {}
   emoticons.each do |key, value|
-    new_hash[key] = {
+    new_hash[key] = value
   binding.pry
 end
 end
